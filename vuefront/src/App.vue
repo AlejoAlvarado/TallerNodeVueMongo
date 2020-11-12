71a0c4359d7e0526866ae298e0df9be7c5af6491
Alejandro Alvarado <Nuestrocanal1@gmail.com>
2020-11-11 22:52:18 -0500

    Added front end project in vue

---
+++ vuefront/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="form-wrapper">
+    <label>Nombre: </label><br />
+    <input v-model="user.name" required /><br />
+
+    <label>Nombre de usuario: </label><br />
+    <input v-model="user.username" required /><br />
+
+    <label>Contraseña: </label><br />
+    <input v-model="user.password" required /><br />
+
+    <label>Foto: </label><br />
+    <input v-model="user.photo" required /><br />
+
+    <label>Activar usuario </label
+    ><input type="checkbox" id="checkbox" v-model="user.active" /><br />
+
+    <button color="success" class="mr-4" @click="sendChangesDepen()">
+      Guardar
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      user: {
+        name: "",
+        username: "",
+        password: "",
+        foto: "",
+        active: false,
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
